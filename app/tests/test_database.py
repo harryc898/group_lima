2acd439bbfdfe1de856d4911ff892388f1fa3924
20cookie05 <m.cook3@nhs.net>
2025-01-10 11:19:12 +0000

    Task 28. Change to test_database.py code. Test for all tests OK

--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -9,22 +9,33 @@ DESCRIPTION:   Suite of tests for testing the dashboards database
 """
 
 import unittest
-#from app import app
+from app import app
 from app.database.controllers import Database
 
 class DatabaseTests(unittest.TestCase):
     """Class for testing database functionality and connection."""
+
     def setUp(self):
-        """Run prior to each test."""
-        self.db_mod = Database()
+        """Set up before each test."""
+        # Tell Flask to use the app
+        self.app = app
+        self.app_context = self.app.app_context()  # Create app context
+        self.app_context.push()  # Activate the context
+        self.db_mod = Database()  # Create the database instance
 
     def tearDown(self):
         """Run post each test."""
-        pass
+        # Pop the application context
+        self.app_context.pop()
 
     def test_get_total_number_items(self):
         """Test that the total number of items returns the correct value."""
-        self.assertEquals(self.db_mod.get_total_number_items(), 8218165, 'Test total items returns correct value')
+        self.assertEqual(self.db_mod.get_total_number_items(),8218165,)
+        'Test total items returns correct value'
+
+    def test_get_total_number_of_GP_practices(self):
+        """Test that the total number of GP practices returns the correct value."""
+        self.assertEqual(self.db_mod.get_total_gp_practices(), 9348, 'Test total GP practices returns correct value')
 
 if __name__ == "__main__":
     unittest.main()
